82bcdba1d529ac1442530bb97ca4bd69b3fbbcff
Suleiman Odetoro <odetoro75@gmail.com>
2023-10-15 21:06:04 +0100

    On Run Event, Store Input Line Commands. Combine commands from single and multi line

--- ProgrammingLanguageEnvironment/Form1.cs
+++ ProgrammingLanguageEnvironment/Form1.cs
@@ -5,6 +5,32 @@ namespace ProgrammingLanguageEnvironment
         public CommandParserForm()
         {
             InitializeComponent();
+            //On Run Button Press event, execute function
+            runButton.Click += RunButton_Click;
+            
+            //
+/*            //create new instance of Command Parser
+            commandParser = new CommandParser();
+            // create new instance of canvas renderer
+            canvasRenderer = new CanvasRenderer(canvas);*/  
+
+
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            //store command strings from multi line and single line input
+            string commands = multiLineCommand.Text + Environment.NewLine + singleLineInput.Text;
+
+            // store parsed commands
+
+            //log parsed commands
+            foreach(var cmd in parsedCommands)
+            {
+                Console.WriteLine(cmd.ToString());
+            }
+        
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
