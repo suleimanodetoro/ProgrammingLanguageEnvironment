8704f3416b82fc07fa7a04ced6536a4391925bdd
Suleiman Odetoro <odetoro75@gmail.com>
2023-10-15 21:25:26 +0100

    Introduced ICommand interface to represent individual drawing commands.

---
+++ ProgrammingLanguageEnvironment/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageEnvironment
+{
+    public interface ICommand
+    {
+        //By passing the CanvasRenderer as an argument to the Execute method,
+        //I'm giving the command a reference to the renderer.
+        //This allows the command to call methods on the renderer to perform drawing operations
+        void Execute(CanvasRenderer renderer);
+
+    }
+}
