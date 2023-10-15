8f42c306369710b3c37f7e5870853debe7df3aaf
Suleiman Odetoro <odetoro75@gmail.com>
2023-10-15 21:59:18 +0100

    Developed CommandParser class to convert textual commands into executable ICommand objects. This bridges the user input with the canvas rendering logic.

---
+++ ProgrammingLanguageEnvironment/CommandParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageEnvironment
+{
+    public class CommandParser
+    {
+        public List<ICommand> ParseCommands(string commandText)
+        {
+            List<ICommand> commands = new List<ICommand>();
+            string[] lines = commandText.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(' ');
+                string commandName = parts[0].ToLower();
+
+                switch (commandName)
+                {
+                    case "moveto":
+                        string[] moveArgs = parts[1].Split(',');
+                        int x = int.Parse(moveArgs[0].Trim());
+                        int y = int.Parse(moveArgs[1].Trim());
+                        //implement moveto command class
+                        break;
+                    case "circle":
+                        int radius = int.Parse(parts[1]);
+                        //implement circle command class
+                        break;
+                    default:
+                        //handle errors later haha
+                        break;
+                }
+            }
+            return commands;
+        }
+    }
+}
