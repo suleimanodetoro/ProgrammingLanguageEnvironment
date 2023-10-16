c32c43509ae1a650a28bae6d8f6eaef39e86584b
Suleiman Odetoro <odetoro75@gmail.com>
2023-10-16 23:42:41 +0100

    Implement Fill command for shape rendering on canvas
    Introduced the Fill command to control the rendering style of shapes:
    
    - **Functionality**:
      - The Fill command allows users to determine whether shapes should be filled with color or simply outlined.
      - It toggles between two modes: filled and outline.
    
    - **Usage**:
      - The command expects a parameter indicating the desired mode.
      - Examples:
        - `fill on` (Shapes will be drawn filled with the current pen color)
        - `fill off` (Shapes will be drawn as outlines using the current pen color)
    
    By adding the Fill command, users are given greater flexibility in their drawings, allowing for diverse visual presentations and increased creative control on the canvas.

---
+++ ProgrammingLanguageEnvironment/FillCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageEnvironment
+{
+    public class FillCommand
+    {
+        public bool IsFillEnabled { get; }
+
+        public FillCommand(string fillOption)
+        {
+            IsFillEnabled = fillOption.ToLower()=="on";
+        }
+
+        public void Execute(CanvasRenderer renderer)
+        {
+            renderer.SetFill(IsFillEnabled);
+        }
+    }
+}
