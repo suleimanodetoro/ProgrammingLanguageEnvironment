f22f315b24d57750e44595882088a5094fe28420
Suleiman Odetoro <odetoro75@gmail.com>
2023-10-22 22:05:33 +0100

    Enhance Command Factory Class with Robust Error Handling
    - Refactored command classes to implement inheritance for cleaner and more maintainable code.
    - Introduced CommandFactory to streamline the creation of command objects based on user input.
    - Added custom exception classes (InvalidCommandException, InvalidParameterException, ExecutionException) to handle various error scenarios.
    - Enhanced CommandFactory to validate parameters for each command and throw relevant exceptions for invalid inputs.
        - Ensured 'moveto' and 'drawto' commands receive valid x,y coordinates.
        - Ensured 'pen', 'circle', 'rect', and 'tri' commands receive valid parameters.
        - Made sure 'reset' and 'clear' commands do not accept any unexpected parameters.

--- ProgrammingLanguageEnvironment/CommandFactory.cs
+++ ProgrammingLanguageEnvironment/CommandFactory.cs
@@ -18,32 +18,78 @@ namespace ProgrammingLanguageEnvironment
             switch (commandName)
             {
                 case "moveto":
+                    //if moveto command receive a single parameter (moveto 10) meaning it does not contain ',' throw an error. It could have have ',' but not have a second paramter too ( move to 100, )
+                    if (args.Length < 1 || !args[0].Contains(','))
+                        throw new InvalidParameterException($"Invalid parameters for 'moveto'. Expected format: x,y but received: {String.Join(" ", args)}.");
                     var moveToArgs = args[0].Split(',');
-                    Point moveToTarget = new Point(int.Parse(moveToArgs[0]), int.Parse(moveToArgs[1]));
-                    return new MoveToCommand(moveToTarget);
+                    //If it has two item seperated with ',' but the items given are invalid, throw error
+                    if (!int.TryParse(moveToArgs[0], out int x) || !int.TryParse(moveToArgs[1], out int y))
+                        throw new InvalidParameterException($"Invalid coordinates provided for 'moveto'. Expected numbers but received: {args[0]}.");
+                    return new MoveToCommand(new Point(x, y));
+
+
                 case "drawto":
+                    // If 'drawto' command receives a single parameter (drawto 10) or does not contain ',' throw an error.
+                    if (args.Length < 1 || !args[0].Contains(','))
+                        throw new InvalidParameterException($"Invalid parameters for 'drawto'. Expected format: x,y but received: {String.Join(" ", args)}.");
                     var drawToArgs = args[0].Split(',');
-                    Point drawToEnd = new Point(int.Parse(drawToArgs[0]), int.Parse(drawToArgs[1]));
-                    return new DrawToCommand(drawToEnd);
+                    // If two items separated by ',' are provided but are invalid, throw error.
+                    if (!int.TryParse(drawToArgs[0], out int endX) || !int.TryParse(drawToArgs[1], out int endY))
+                        throw new InvalidParameterException($"Invalid coordinates provided for 'drawto'. Expected numbers but received: {args[0]}.");
+                    return new DrawToCommand(new Point(endX, endY));
+
+
                 case "pen":
+                    // Check if the pen command has received a valid color.
+                    if (args.Length < 1 || String.IsNullOrEmpty(args[0]))
+                        throw new InvalidParameterException("Invalid color provided for 'pen' command.");
                     return new PenCommand(args[0]);
+
+
                 case "rect":
+                    // If 'rect' command receives a single parameter (rect 10,20) or does not contain ',' throw an error.
+                    if (args.Length < 1 || !args[0].Contains(','))
+                        throw new InvalidParameterException($"Invalid parameters for 'rect'. Expected format: width,height but received: {String.Join(" ", args)}.");
                     var rectArgs = args[0].Split(',');
-                    int width = int.Parse(rectArgs[0]);
-                    int height = int.Parse(rectArgs[1]);
-                    return new RectangleCommand(width, height);
+                    // If two items separated by ',' are provided but are invalid, throw error.
+                    if (!int.TryParse(rectArgs[0], out int rectWidth) || !int.TryParse(rectArgs[1], out int rectHeight))
+                        throw new InvalidParameterException($"Invalid dimensions provided for 'rect'. Expected numbers but received: {args[0]}.");
+                    return new RectangleCommand(rectWidth, rectHeight);
+
+
                 case "circle":
-                    int radius = int.Parse(args[0]);
-                    return new CircleCommand(radius);
+                    // Check if circle command receives a valid radius.
+                    if (args.Length < 1 || !int.TryParse(args[0], out int circleRadius))
+                        throw new InvalidParameterException($"Invalid radius provided for 'circle'. Expected number but received: {args[0]}.");
+                    return new CircleCommand(circleRadius);
+
+
                 case "tri":
-                    int sideLength = int.Parse(args[0]);
-                    return new TriangleCommand(sideLength);
+                    // Check if triangle command receives a valid side length.
+                    if (args.Length < 1 || !int.TryParse(args[0], out int triangleSide))
+                        throw new InvalidParameterException($"Invalid side length provided for 'tri'. Expected number but received: {args[0]}.");
+                    return new TriangleCommand(triangleSide);
+
+
                 case "fill":
+                    if (args.Length < 1 || (args[0] != "on" && args[0] != "off"))
+                        throw new InvalidParameterException($"Invalid parameter for 'fill'. Expected 'on' or 'off' but received: {String.Join(" ", args)}.");
+
                     return new FillCommand(args[0] == "on");
                 case "reset":
+                    // The 'reset' command should not have any parameters.
+                    if (args.Length > 0)
+                        throw new InvalidParameterException($"The 'reset' command does not expect any parameters but received: {String.Join(" ", args)}.");
                     return new ResetCommand();
+
+
                 case "clear":
+                    // The 'clear' command should not have any parameters.
+                    if (args.Length > 0)
+                        throw new InvalidParameterException($"The 'clear' command does not expect any parameters but received: {String.Join(" ", args)}.");
                     return new ClearCommand();
+
+
                 default:
                     throw new InvalidCommandException($"Unknown command \"{commandName}\".");
             }
