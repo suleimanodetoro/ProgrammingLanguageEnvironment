7e166b0364599b5bcb3d29bdfe3081641e26e370
Suleiman Odetoro <odetoro75@gmail.com>
2023-10-22 20:47:46 +0100

    Add Custom Exception Claases for Command Error Handling
    - Introduced `InvalidCommandException` to handle unrecognized or invalid commands.
    - Added `InvalidParameterException` to manage cases with incorrect command parameters.
    - Added`ExecutionException` for errors occurring during command execution.

---
+++ ProgrammingLanguageEnvironment/CommandErrorExceptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageEnvironment
+{
+    // Exception for invalid commands (Inherit from sytem exception definition)
+    public class InvalidCommandException : Exception
+    {
+        public InvalidCommandException(string message) : base(message) { }
+    }
+
+    // Exception for invalid parameters (Inherit from sytem exception definition)
+    public class InvalidParameterException : Exception
+    {
+        public InvalidParameterException(string message) : base(message) { }
+    }
+
+    // Exception for issues during command execution (Inherit from sytem exception definition)
+    public class ExecutionException : Exception
+    {
+        public ExecutionException(string message) : base(message) { }
+    }
+}
