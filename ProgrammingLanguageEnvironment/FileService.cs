840b4d5a6d21b19370f65733ee98b4dd233ac516
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-04 13:39:05 +0000

    Implement Save Button Functionality

---
+++ ProgrammingLanguageEnvironment/FileService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageEnvironment
+{
+    public interface IFileService
+    {
+        void SaveCommandsToFile(string filePath, IEnumerable<string> commands);
+
+    }
+
+    public class FileService : IFileService
+    {
+        // Method to save commands to a text file
+        public void SaveCommandsToFile(string filePath, IEnumerable<string> commands)
+        {
+            // Write all lines to the file
+            File.WriteAllLines(filePath, commands);
+        }
+    }
+}
