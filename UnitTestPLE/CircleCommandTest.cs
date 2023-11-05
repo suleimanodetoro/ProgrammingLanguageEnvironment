9f0372baa32072feb182f63aaefbfa7e7d70dd31
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-05 12:14:46 +0000

    Full XML documentation for Circle Command Unit Tests

--- UnitTestPLE/CircleCommandTest.cs
+++ UnitTestPLE/CircleCommandTest.cs
@@ -8,11 +8,18 @@ using System.Threading.Tasks;
 
 namespace UnitTestPLE
 {
+    /// <summary>
+    /// Contains tests for the CircleCommand class to ensure its proper functionality.
+    /// </summary>
     [TestClass]
     public class CircleCommandTest
     {
-        // Test to ensure that a CircleCommand object is correctly instantiated
-        // with a valid radius.
+        /// <summary>
+        /// Tests whether a CircleCommand object is correctly instantiated with a valid radius.
+        /// </summary>
+        /// <remarks>
+        /// A CircleCommand is expected to use the valid radius provided during instantiation.
+        /// </remarks>
         [TestMethod]
         public void CircleCommand_ValidRadius_CreatesCommand()
         {
@@ -23,16 +30,22 @@ namespace UnitTestPLE
             // Act: Create a new CircleCommand with the specified radius.
             var circleCommand = new CircleCommand(radius);
 
-            // Assert: Check if the created command has the correct radius.
+            // Assert: The radius stored in CircleCommand should match the radius provided.
             Assert.AreEqual(radius, circleCommand.Radius, "The radius stored in CircleCommand should match the radius provided.");
         }
 
-        // Test to verify that the Execute method of CircleCommand correctly
-        // calls the DrawCircle method of a CanvasRenderer with the right radius.
+        /// <summary>
+        /// Tests whether the Execute method of CircleCommand correctly calls the DrawCircle
+        /// method of a CanvasRenderer with the correct radius.
+        /// </summary>
+        /// <remarks>
+        /// This test uses a mock renderer to verify that the DrawCircle method is called
+        /// with the correct radius when the Execute method of the CircleCommand is called.
+        /// </remarks>
         [TestMethod]
         public void CircleCommand_Execute_CallsDrawCircleWithCorrectRadius()
         {
-            // Arrange: Initialize the necessary objects and mocks.
+            // Arrange: Initialize the necessary objects and mock CanvasRenderer
             int radius = 10;
             var circleCommand = new CircleCommand(radius);
             var mockRenderer = new Mock<ICanvasRenderer>();
@@ -46,8 +59,17 @@ namespace UnitTestPLE
             mockRenderer.Verify(r => r.DrawCircle(radius), Times.Once(), "DrawCircle should be called once with the correct radius.");
         }
 
-        // Test to confirm that an InvalidParameterException is thrown when a
-        // CircleCommand is instantiated with a negative radius.
+
+
+        /// <summary>
+        /// Tests whether an InvalidParameterException is thrown when a CircleCommand is
+        /// instantiated with a negative radius.
+        /// </summary>
+        /// <remarks>
+        /// CircleCommand should not allow negative radii. This test ensures that an
+        /// InvalidParameterException is thrown when trying to instantiate a CircleCommand
+        /// with a negative radius.
+        /// </remarks>
         [TestMethod]
         [ExpectedException(typeof(InvalidParameterException), "A negative radius should cause an InvalidParameterException to be thrown.")]
         public void CircleCommand_NegativeRadius_ThrowsInvalidParameterException()
