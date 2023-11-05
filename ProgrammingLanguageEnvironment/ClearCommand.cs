612695755ac589426e635d1edccddf944703999d
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-05 13:34:01 +0000

    Add XML documentation for Clear Command Class

--- ProgrammingLanguageEnvironment/ClearCommand.cs
+++ ProgrammingLanguageEnvironment/ClearCommand.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
+    /// <summary>
+    /// Represents a command to clear the canvas.
+    /// </summary>
     public class ClearCommand : Command
     {
+        /// <summary>
+        /// Executes the clear command on a given renderer, which clears the canvas.
+        /// </summary>
+        /// <param name="renderer">The canvas renderer that will have its canvas cleared.</param>
         public override void Execute(ICanvasRenderer renderer)
         {
             renderer.ClearCanvas();
