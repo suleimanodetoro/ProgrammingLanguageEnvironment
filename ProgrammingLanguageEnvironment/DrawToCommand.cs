15e62d535a424584308fc02547f53118016a80e4
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-05 13:29:34 +0000

    Add DrawTo Class XML Documentation

--- ProgrammingLanguageEnvironment/DrawToCommand.cs
+++ ProgrammingLanguageEnvironment/DrawToCommand.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
+    /// <summary>
+    /// Represents a command to draw a line to a specified endpoint on a canvas.
+    /// </summary>
     public class DrawToCommand : Command
     {
         private Point endPoint;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DrawToCommand"/> class with a specified endpoint.
+        /// </summary>
+        /// <param name="endPoint">The endpoint where the line should be drawn to.</param>
+        /// <exception cref="InvalidParameterException">
+        /// Thrown when the coordinates of the endpoint are negative.
+        /// </exception>
         public DrawToCommand(Point endPoint)
         {
             // Check if the provided Point is valid. For example, we can check if X and Y are non-negative.
@@ -20,8 +30,16 @@ namespace ProgrammingLanguageEnvironment
             this.endPoint = endPoint;
         }
 
+        /// <summary>
+        /// Gets the endpoint where the line should be drawn to.
+        /// </summary>
         public Point EndPoint => endPoint;
 
+
+        /// <summary>
+        /// Executes the draw to command on a given renderer.
+        /// </summary>
+        /// <param name="renderer">The canvas renderer where the line will be drawn to the endpoint.</param>
         public override void Execute(ICanvasRenderer renderer)
         {
             renderer.DrawLine(endPoint);
