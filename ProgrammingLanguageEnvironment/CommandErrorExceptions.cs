befeea59151bc3da544d1983a2e8094af18e4070
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-05 13:19:34 +0000

    Add complete XML documentation for Error Handling Helper class

--- ProgrammingLanguageEnvironment/CommandErrorExceptions.cs
+++ ProgrammingLanguageEnvironment/CommandErrorExceptions.cs
@@ -6,21 +6,45 @@ using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
+    /// <summary>
+    /// Represents errors that occur due to an invalid command being issued to the system.
+    /// Used extensively across application for exception handling
+    /// </summary>
     // Exception for invalid commands (Inherit from sytem exception definition)
     public class InvalidCommandException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the InvalidCommandException class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+
         public InvalidCommandException(string message) : base(message) { }
     }
 
-    // Exception for invalid parameters (Inherit from sytem exception definition)
+
+    /// <summary>
+    /// Represents errors that occur due to invalid parameters being passed to a command.
+    /// </summary>
     public class InvalidParameterException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the InvalidParameterException class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+
         public InvalidParameterException(string message) : base(message) { }
     }
 
-    // Exception for issues during command execution (Inherit from sytem exception definition)
+/// <summary>
+    /// Represents errors that occur during the execution of a command.
+    /// </summary>
     public class ExecutionException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the ExecutionException class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+
         public ExecutionException(string message) : base(message) { }
     }
 }
