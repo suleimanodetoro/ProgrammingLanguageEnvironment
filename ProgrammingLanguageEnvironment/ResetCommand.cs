9a6277adfccbf92bb84486db897d78e2df3f4eb8
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-05 13:50:03 +0000

    Add XML documentation for Reset command

--- ProgrammingLanguageEnvironment/ResetCommand.cs
+++ ProgrammingLanguageEnvironment/ResetCommand.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
+    /// <summary>
+    /// Represents a command to reset the drawing cursor to the origin (0,0) on the canvas.
+    /// </summary>
+
     public class ResetCommand : Command
     {
+        /// <summary>
+        /// Executes the reset command on the provided canvas renderer.
+        /// </summary>
+        /// <param name="renderer">The canvas renderer on which to reset the drawing position.</param>
+        /// <remarks>
+        /// The execution of this command will move the current drawing position
+        /// of the renderer back to the origin point (0,0) of the canvas.
+        /// </remarks>
         public override void Execute(ICanvasRenderer renderer)
         {
             renderer.MoveTo(new Point(0,0));
