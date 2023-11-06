6387ae735cca726b223cb2495650fe85d42ef9bd
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-06 07:46:44 +0000

    Add XML documentation for MoveTo Command Class

--- ProgrammingLanguageEnvironment/MoveTo.cs
+++ ProgrammingLanguageEnvironment/MoveTo.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
+    /// <summary>
+    /// class represents a command to move the pen to a specified position on the canvas.
+    /// </summary>
     public class MoveToCommand: Command
     {
+        /// <summary>
+        /// target point variable definition
+        /// </summary>
         private Point targetPosition;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoveToCommand"/> class with the target position.
+        /// </summary>
+        /// <param name="target">The <see cref="Point"/> representing the target position.</param>
+        /// <exception cref="InvalidParameterException">
+        /// Thrown when the provided coordinates are negative.
+        /// </exception>
 
         public MoveToCommand(Point target)
         {
@@ -20,11 +33,23 @@ namespace ProgrammingLanguageEnvironment
             this.targetPosition = target; 
         }
 
+        /// <summary>
+        /// Gets the target position where the pen will be moved.
+        /// </summary>
+        public Point TargetPosition => targetPosition;
+
+
+        /// <summary>
+        /// Executes the command to move the pen to the target position on the provided canvas renderer.
+        /// </summary>
+        /// <param name="renderer">The canvas renderer on which to move the pen.</param>
+        /// <remarks>
+        /// The method calls the <c>MoveTo</c> method on the renderer with the target position.
+        /// </remarks>
         public override void Execute(ICanvasRenderer renderer)
         {
             renderer.MoveTo(targetPosition);
         }
-        public Point TargetPosition => targetPosition;
 
     }
 }
