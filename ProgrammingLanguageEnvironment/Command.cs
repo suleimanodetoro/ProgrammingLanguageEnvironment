58ef1f7509e5c620375228c814a318bad8d7d1af
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-08 21:13:16 +0000

    Add XML to Command Abstract class

--- ProgrammingLanguageEnvironment/Command.cs
+++ ProgrammingLanguageEnvironment/Command.cs
@@ -7,8 +7,16 @@ using System.Drawing;
 
 namespace ProgrammingLanguageEnvironment
 {
+    /// <summary>
+    /// Represents the base class for all drawing commands.
+    /// This is an abstract class that defines a common interface for all commands.
+    /// </summary>
     public abstract class Command
     {
+        /// <summary>
+        /// When implemented in a derived class, executes the command using the provided canvas renderer.
+        /// </summary>
+        /// <param name="renderer">The canvas renderer that will execute the command.</param>
         public abstract void Execute(ICanvasRenderer renderer);
     }
 }
