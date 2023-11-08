2dae13d99c4535616bb3a338a99d4e2b479001e2
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-08 21:35:18 +0000

    add xml for command service

--- ProgrammingLanguageEnvironment/CommandService.cs
+++ ProgrammingLanguageEnvironment/CommandService.cs
@@ -6,39 +6,82 @@ using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
+    /// <summary>
+    /// Defines the interface for a service that executes and manages drawing commands.
+    /// </summary>
     public interface ICommandService
     {
+        /// <summary>
+        /// Executes a series of commands on a canvas.
+        /// </summary>
+        /// <param name="commands">The string containing the commands to be executed.</param>
         void ExecuteCommands(string commands);
+        /// <summary>
+        /// Checks the syntax of the commands without executing them.
+        /// </summary>
+        /// <param name="commands">The string containing the commands to be checked.</param>
+        /// <param name="errorMessage">Outputs an error message if the syntax is incorrect.</param>
+        /// <returns>True if the syntax is correct; otherwise, false.</returns>
         bool CheckSyntax(string commands, out string errorMessage);
-        void DisplayMessage(string message); // Declare the method in the interface
+
+        /// <summary>
+        /// Displays a message on the canvas.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        void DisplayMessage(string message); 
+
+        /// <summary>
+        /// Clears the canvas.
+        /// </summary>
         void ClearCanvas(); // to help clear the canvas on checks
 
     }
 
-
+    /// <summary>
+    /// Provides services for executing and managing drawing commands.
+    /// </summary>
     public class CommandService : ICommandService
         {
-            private readonly CommandParser _commandParser;
-            private readonly CanvasRenderer _canvasRenderer;
+        private readonly CommandParser _commandParser;
+        private readonly CanvasRenderer _canvasRenderer;
 
-            public CommandService(CommandParser commandParser, CanvasRenderer canvasRenderer)
-            {
-                _commandParser = commandParser;
-                _canvasRenderer = canvasRenderer;
-            }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandService"/> class with the specified command parser and canvas renderer.
+        /// </summary>
+        /// <param name="commandParser">The parser used to parse the commands.</param>
+        /// <param name="canvasRenderer">The renderer used to draw on the canvas.</param>
+        public CommandService(CommandParser commandParser, CanvasRenderer canvasRenderer)
+        {
+          _commandParser = commandParser;
+          _canvasRenderer = canvasRenderer;
+        }
+        /// <summary>
+        /// Clears the drawing canvas.
+        /// </summary>
         public void ClearCanvas()
         {
             // Delegate to the canvas renderer's method to clear the canvas
             _canvasRenderer.ClearCanvas();
         }
 
+        /// <summary>
+        /// executes a series of drawing commands.
+        /// </summary>
+        /// <param name="commands">The string containing the commands to be executed.</param>
         public void ExecuteCommands(string commands)
-            {
+        {
                 var parsedCommands = _commandParser.ParseCommands(commands);
                 _canvasRenderer.ExecuteCommands(parsedCommands);
-            }
+        }
 
-            public bool CheckSyntax(string commands, out string? errorMessage)
+        /// <summary>
+        /// Checks the syntax of a series of drawing commands.
+        /// </summary>
+        /// <param name="commands">The string containing the commands to be checked.</param>
+        /// <param name="errorMessage">When this method returns, contains the error message if the syntax is incorrect; otherwise, null.</param>
+        /// <returns>True if the syntax is correct; otherwise, false.</returns>
+        public bool CheckSyntax(string commands, out string? errorMessage)
             {
             errorMessage = null;
             try
@@ -53,7 +96,12 @@ namespace ProgrammingLanguageEnvironment
             }
         }
 
-            public void DisplayMessage(string message)
+        /// <summary>
+        /// Displays a text message on the canvas.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+
+        public void DisplayMessage(string message)
             {
             
                 // Delegate to the canvas renderer's method to display the message
