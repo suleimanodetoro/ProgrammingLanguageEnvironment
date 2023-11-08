50124ba39e2aa8628e264c1dd067b0ef8e1fce26
Suleiman Odetoro <odetoro75@gmail.com>
2023-11-08 21:18:39 +0000

    Add XML and comments for Circle Command

--- ProgrammingLanguageEnvironment/CircleCommand.cs
+++ ProgrammingLanguageEnvironment/CircleCommand.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
+    /// <summary>
+    /// This class represents a command to draw a circle on the canvas.
+    /// </summary>
     public class CircleCommand : Command
     {
+        /// <summary>
+        /// The radius of the circle to be drawn.
+        /// </summary>
         private int radius;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CircleCommand"/> class with the specified radius.
+        /// </summary>
+        /// <param name="radius">The radius of the circle. Must be greater than zero.</param>
+        /// <exception cref="InvalidParameterException">Thrown when the radius is less than or equal to zero.</exception>
         public CircleCommand(int radius)
         {
             if (radius <= 0)
@@ -18,6 +29,9 @@ namespace ProgrammingLanguageEnvironment
             this.radius = radius;
         }
 
+        /// <summary>
+        /// Getter the radius of the circle.
+        /// </summary>
         public int Radius => radius;
 
         public override void Execute(ICanvasRenderer renderer)
