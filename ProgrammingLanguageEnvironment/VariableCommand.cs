0171a1beb237327eea22b6db62fce998a24dd6de
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-09 16:43:35 +0000

    Add Variable Command Class without ExecutionContext Integration

---
+++ ProgrammingLanguageEnvironment/VariableCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageEnvironment
+{
+    public class VariableCommand : Command
+    {
+        private string variableName;
+        private int value;
+
+        public VariableCommand(string variableName, int value)
+        {
+            this.variableName = variableName;
+            this.value = value;
+        }
+
+        public override void Execute(ICanvasRenderer renderer, ExecutionContext context)
+        {
+/*            context.SetVariable(variableName, value);
+*/        }
+    }
+}
