d103639eeff5392667d9b21cd33b912ea9d31bb4
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-09 18:49:41 +0000

    Create Colour Command Class for changing line colours with rgb color codes
    -Saw this in the demo, implementing as additional functionality

---
+++ ProgrammingLanguageEnvironment/ColourCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageEnvironment
+{
+    public class ColourCommand : Command
+    {
+        private Color color;
+
+        public ColourCommand(Color color)
+        {
+            this.color = color;
+        }
+
+        public override void Execute(ICanvasRenderer renderer, ExecutionContext context)
+        {
+            renderer.SetPenColor(color);
+        }
+    }
+}
