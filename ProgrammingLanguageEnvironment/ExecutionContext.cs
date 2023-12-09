ce1f774a6e16d146ef6a4af3273df5e102a09007
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-09 16:58:37 +0000

    Enhance ExecutionContext with Variable Handling Functions
    In this commit, the ExecutionContext has been updated to include functions for variable management, namely setVariable and GetVariableValue. This is for allowing commands to access and manipulate variables within a shared execution context.

--- ProgrammingLanguageEnvironment/ExecutionContext.cs
+++ ProgrammingLanguageEnvironment/ExecutionContext.cs
@@ -8,5 +8,41 @@ namespace ProgrammingLanguageEnvironment
 {
     public class ExecutionContext
     {
+        public Dictionary<string, int> Variables { get; private set; } = new Dictionary<string, int>();
+        public List<Command> Commands { get; set; } = new List<Command>();
+        public int ProgramCounter { get; set; } = 0;
+
+        // Add methods for variable and loop handling
+        public void SetVariable(string name, int value)
+        {
+            Variables[name] = value;
+        }
+
+        public int GetVariableValue(string variableOrConstant)
+        {
+            if (int.TryParse(variableOrConstant, out int constantValue))
+            {
+                return constantValue; // It's a constant
+            }
+            else if (Variables.TryGetValue(variableOrConstant, out int variableValue))
+            {
+                return variableValue; // It's a variable
+            }
+            throw new Exception($"Variable '{variableOrConstant}' not found or invalid constant.");
+        }
+
+        public int GetVariable(string name)
+        {
+            if (Variables.TryGetValue(name, out int value))
+            {
+                return value;
+            }
+            throw new Exception($"Variable '{name}' not found.");
+        }
+       
     }
+
+   
+
+
 }
