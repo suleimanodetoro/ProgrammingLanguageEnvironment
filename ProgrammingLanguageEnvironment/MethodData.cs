27c4a800530b572a78e9f773c0eef926a35f17fe
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-13 17:12:50 +0000

    Add MethodData class for method definition storage ->  will serve as a data structure for storing method definitions parsed from the command input.

---
+++ ProgrammingLanguageEnvironment/MethodData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageEnvironment
+{
+    public class MethodData
+    {
+        public string MethodName { get; set; }
+        public List<string> Parameters { get; set; }
+        public List<Command> Commands { get; set; }
+
+        public MethodData(string methodName, List<string> parameters)
+        {
+            MethodName = methodName;
+            Parameters = parameters;
+            Commands = new List<Command>();
+        }
+    }
+
+}
