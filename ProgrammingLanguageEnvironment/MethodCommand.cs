219f88e96eeda211841598b44a5ab8cd9de18f93
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-13 17:18:06 +0000

    Implement MethodCommand for method execution Needed work: Extend context class for method functionality

---
+++ ProgrammingLanguageEnvironment/MethodCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageEnvironment
+{
+    public class MethodCommand : Command
+    {
+        public string MethodName { get; }
+        public List<string> Parameters { get; }
+        public List<Command> MethodBody { get; }
+
+        public MethodCommand(string methodName, List<string> parameters, List<Command> methodBody)
+        {
+            MethodName = methodName;
+            Parameters = parameters;
+            MethodBody = methodBody;
+        }
+
+        public override void Execute(ICanvasRenderer renderer, ExecutionContext context)
+        {
+/*            context.AddMethod(MethodName, this);
+*/        }
+
+    }
+
+}
