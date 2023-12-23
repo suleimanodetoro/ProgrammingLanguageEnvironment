9bf1e873ac4c42a7c7442c8b0e253326884c1181
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-23 16:07:31 +0000

    Fix bug: Pointer not moving when you drawto command

--- ProgrammingLanguageEnvironment/DrawToCommand.cs
+++ ProgrammingLanguageEnvironment/DrawToCommand.cs
@@ -22,12 +22,19 @@ namespace ProgrammingLanguageEnvironment
 
         public override void Execute(ICanvasRenderer renderer, ExecutionContext context)
         {
+            // Extract the x and y values possibly from variables or direct numbers
             int x = context.GetVariableValue(xParameter);
             int y = context.GetVariableValue(yParameter);
             Point endPoint = new Point(x, y);
 
+            // Draw the line on the canvas
             renderer.DrawLine(context.CurrentPosition, endPoint, context.CurrentColor);
+
+            // Update the current position in the context to the end point of the line
             context.CurrentPosition = endPoint;
+
+            // Update the pointer position to reflect the new position
+            renderer.DrawPointer(endPoint);
         }
     }
 }
