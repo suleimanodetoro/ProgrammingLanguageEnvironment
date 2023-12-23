146f0f9074eea37652768409d4a0aac11f1b52da
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-23 14:24:50 +0000

    Bug Fix For ICommand and ICommandService confusion

--- ProgrammingLanguageEnvironment/ICommand.cs
+++ ProgrammingLanguageEnvironment/ICommand.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
-    public interface ICommandService
+    public interface ICommand
     {
-        void ExecuteCommands(string commands);
-        Task ExecuteCommandsParallel(IEnumerable<string> commandSets);
-        bool CheckSyntax(string commands, out string errorMessage);
-        void DisplayMessage(string message);
-        void ClearCanvas();
+        //By passing the CanvasRenderer as an argument to the Execute method,
+        //I'm giving the command a reference to the renderer.
+        //This allows the command to call methods on the renderer to perform drawing operations
+        void Execute(CanvasRenderer renderer);
+
     }
 }
--- ProgrammingLanguageEnvironment/ICommandService.cs
+++ ProgrammingLanguageEnvironment/ICommandService.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProgrammingLanguageEnvironment
+﻿namespace ProgrammingLanguageEnvironment
 {
-    internal class ICommandService
+    public interface ICommandService
     {
+        void ExecuteCommands(string commands);
+        Task ExecuteCommandsParallel(IEnumerable<string> commandSets);
+        bool CheckSyntax(string commands, out string errorMessage);
+        void DisplayMessage(string message);
+        void ClearCanvas();
     }
 }
