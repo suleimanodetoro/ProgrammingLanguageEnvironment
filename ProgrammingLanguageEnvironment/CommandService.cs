9c4db2cf97c5c04bb7a82f9a07c7cadbbf0946e9
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-23 14:26:50 +0000

    Modify command service class to use interface created, and support thread safe operations

--- ProgrammingLanguageEnvironment/CommandService.cs
+++ ProgrammingLanguageEnvironment/CommandService.cs
@@ -1,84 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
-    
-
-    /// <summary>
-    /// Provides services for executing and managing drawing commands.
-    /// </summary>
-    public class CommandService
-        {
+    public class CommandService : ICommandService
+    {
         private readonly CommandParser _commandParser;
         private readonly ICanvasRenderer _canvasRenderer;
 
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CommandService"/> class with the specified command parser and canvas renderer.
-        /// </summary>
-        /// <param name="commandParser">The parser used to parse the commands.</param>
-        /// <param name="canvasRenderer">The renderer used to draw on the canvas.</param>
         public CommandService(CommandParser commandParser, ICanvasRenderer canvasRenderer)
         {
-          _commandParser = commandParser;
-          _canvasRenderer = canvasRenderer;
-        }
-        /// <summary>
-        /// Clears the drawing canvas.
-        /// </summary>
-        public void ClearCanvas()
-        {
-            // Delegate to the canvas renderer's method to clear the canvas
-            _canvasRenderer.ClearCanvas();
+            _commandParser = commandParser;
+            _canvasRenderer = canvasRenderer;
         }
 
-        /// <summary>
-        /// executes a series of drawing commands.
-        /// </summary>
-        /// <param name="commands">The string containing the commands to be executed.</param>
         public void ExecuteCommands(string commands)
         {
-                var parsedCommands = _commandParser.ParseCommands(commands);
-                _canvasRenderer.ExecuteCommands(parsedCommands);
+            ExecutionContext context = new ExecutionContext();
+            var parsedCommands = _commandParser.ParseCommands(commands);
+            foreach (var command in parsedCommands)
+            {
+                command.Execute(_canvasRenderer, context);
+            }
         }
 
-        /// <summary>
-        /// Checks the syntax of a series of drawing commands.
-        /// </summary>
-        /// <param name="commands">The string containing the commands to be checked.</param>
-        /// <param name="errorMessage">When this method returns, contains the error message if the syntax is incorrect; otherwise, null.</param>
-        /// <returns>True if the syntax is correct; otherwise, false.</returns>
-        public bool CheckSyntax(string commands, out string? errorMessage)
+
+        // New method for executing multiple command sets in parallel
+        public async Task ExecuteCommandsParallel(IEnumerable<string> commandSets)
+        {
+            var tasks = commandSets.Select(commands =>
+            {
+                return Task.Run(() =>
+                {
+                    ExecuteCommands(commands);
+                });
+            });
+
+            await Task.WhenAll(tasks);
+        }
+
+        public bool CheckSyntax(string commands, out string errorMessage)
         {
             errorMessage = null;
             try
-        {
+            {
                 _commandParser.ParseCommands(commands);
-                return true; // Syntax is correct
-        }
+                return true;
+            }
             catch (Exception ex)
             {
-                errorMessage = ex.Message; // Capture the exception message
-                return false; // Syntax is incorrect
-        }
-        }
-
-        /// <summary>
-        /// Displays a text message on the canvas.
-        /// </summary>
-        /// <param name="message">The message to be displayed.</param>
-
-        public void DisplayMessage(string message)
-            {
-            
-                // Delegate to the canvas renderer's method to display the message
-                _canvasRenderer.DisplayTextOnCanvas(message);
+                errorMessage = ex.Message;
+                return false;
             }
         }
+
+        public void DisplayMessage(string message)
+        {
+            _canvasRenderer.DisplayTextOnCanvas(message);
+        }
+
+        public void ClearCanvas()
+        {
+            _canvasRenderer.ClearCanvas();
+        }
     }
-
-
+}
