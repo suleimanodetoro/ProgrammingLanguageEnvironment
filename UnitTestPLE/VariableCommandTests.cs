1f27e92b7065e343e1e9b887f827c00ca3fdc4c7
Suleiman Odetoro <odetoro75@gmail.com>
2023-12-28 16:09:17 +0000

    Initial Iteration of Variable Tests, details:
    ToDo: Verify tests work, bug from previous tests (old iteration of the application), affecting execution of this test cases
    
    Test 1: Check the varibales are created
    Test 2: Check that variables are assigned to given values
    Test 3: Test variable expressions

---
+++ UnitTestPLE/VariableCommandTests.cs
@@ -0,0 +1,71 @@
+﻿using Moq;
+using ProgrammingLanguageEnvironment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace UnitTestPLE
+{
+    [TestClass]
+    public class VariableTests
+    {
+        private Mock<ICanvasRenderer> mockRenderer;
+        private ProgrammingLanguageEnvironment.ExecutionContext context;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockRenderer = new Mock<ICanvasRenderer>();
+            context = new ProgrammingLanguageEnvironment.ExecutionContext();
+        }
+
+
+        //The following is for testing varibale command 
+        [TestMethod]
+        public void Should_SetVariable_When_VariableCommandExecuted()
+        {
+            // Arrange
+            var command = new VariableCommand("x", 10);
+
+            // Act
+            command.Execute(mockRenderer.Object, context);
+
+            // Assert
+            Assert.AreEqual(10, context.Variables["x"]);
+        }
+
+
+        //the following is for testing is variables are assigned to values
+        [TestMethod]
+        public void Should_UpdateVariable_When_AssignmentCommandExecuted()
+        {
+            // Arrange
+            context.SetVariable("x", 10); // Initial value
+            var command = new AssignmentCommand("x", "20"); // New value as direct integer
+
+            // Act
+            command.Execute(mockRenderer.Object, context);
+
+            // Assert
+            Assert.AreEqual(20, context.Variables["x"]);
+        }
+
+
+        //the following is for testing expression or redefinitions work
+        [TestMethod]
+        public void Should_EvaluateExpression_When_AssignmentCommandExecuted()
+        {
+            // Arrange
+            context.SetVariable("x", 10); // Initial value
+            var command = new AssignmentCommand("x", "x + 15"); // Increment x by 15
+
+            // Act
+            command.Execute(mockRenderer.Object, context);
+
+            // Assert
+            Assert.AreEqual(25, context.Variables["x"]);
+        }
+
+
+
+    }
+}
