d45b8dec31431b5999b375a942050270034bc3f1
Suleiman Odetoro <odetoro75@gmail.com>
2024-01-03 14:58:29 +0000

    Implement unit tests for Syntax Checking
    - Test to make unknown commands throw appropriate error
    - Test to throw error when unclosed loop is encountered

---
+++ UnitTestPLE/SyntaxCheckingTests.cs
@@ -0,0 +1,61 @@
+﻿
+using ProgrammingLanguageEnvironment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTestPLE
+{
+    [TestClass]
+    public class SyntaxCheckingTests
+    {
+        private CommandParser _commandParser;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _commandParser = new CommandParser();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CommandException))]
+        public void ParseCommands_UnknownCommand_ThrowsCommandExceptionWithLineInfo()
+        {
+            try
+            {
+                string input = "unknownCommand 10,20\nvalidCommand";
+                _commandParser.ParseCommands(input);
+            }
+            catch (CommandException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("line 1"), "Error message should contain line number.");
+                throw; // Rethrow to satisfy ExpectedException
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CommandException))]
+        public void ParseCommands_UnclosedLoop_ThrowsCommandExceptionWithLineInfo()
+        {
+            try
+            {
+                string input = "while condition\nvalidCommand";
+                _commandParser.ParseCommands(input);
+            }
+            catch (CommandException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("Unclosed loop"), "Error message should mention unclosed loop.");
+                Assert.IsTrue(ex.Message.Contains("line 1"), "Error message should contain line number.");
+                throw; // Rethrow to satisfy ExpectedException
+            }
+        }
+
+    }
+
+
+
+
+}
