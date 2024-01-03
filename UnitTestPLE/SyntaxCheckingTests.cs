0e07ecc2bc5cad2f82528d2dcd57efd8cb7d9907
Suleiman Odetoro <odetoro75@gmail.com>
2024-01-03 15:04:36 +0000

    Add XML documentation and inline comments for syntax checking unit tests

--- UnitTestPLE/SyntaxCheckingTests.cs
+++ UnitTestPLE/SyntaxCheckingTests.cs
@@ -9,10 +9,19 @@ using System.Threading.Tasks;
 
 namespace UnitTestPLE
 {
+    /// <summary>
+    /// Contains tests for checking the syntax parsing functionality of the CommandParser class.
+    /// Ensures that exceptions are thrown and handled correctly for various erroneous inputs.
+    /// </summary>
     [TestClass]
     public class SyntaxCheckingTests
     {
+        // CommandParser instance used in test methods.
         private CommandParser _commandParser;
+
+        /// <summary>
+        /// Initializes a new instance of CommandParser before each test is run.
+        /// </summary>
 
         [TestInitialize]
         public void Setup()
@@ -20,10 +29,20 @@ namespace UnitTestPLE
             _commandParser = new CommandParser();
         }
 
+        /// <summary>
+        /// Tests the parsing of commands with an unknown command to ensure that the proper exception is thrown with correct line information.
+        /// </summary>
+        /// <remarks>
+        /// The method inputs a string with an unknown command. It expects a CommandException to be thrown,
+        /// and the exception message should contain information about the line number of the error.
+        /// </remarks>
+        /// 
+
         [TestMethod]
         [ExpectedException(typeof(CommandException))]
         public void ParseCommands_UnknownCommand_ThrowsCommandExceptionWithLineInfo()
         {
+
             try
             {
                 string input = "unknownCommand 10,20\nvalidCommand";
@@ -36,6 +55,14 @@ namespace UnitTestPLE
             }
         }
 
+
+        /// <summary>
+        /// Tests parsing of commands to ensure that unclosed loops are correctly identified and reported with line information in the exception.
+        /// </summary>
+        /// <remarks>
+        /// The method inputs a string that simulates an unclosed loop condition. It expects a CommandException to be thrown,
+        /// and the exception message should contain details about the unclosed loop and the line number of the error.
+        /// </remarks>
         [TestMethod]
         [ExpectedException(typeof(CommandException))]
         public void ParseCommands_UnclosedLoop_ThrowsCommandExceptionWithLineInfo()
